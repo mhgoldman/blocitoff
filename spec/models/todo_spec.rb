1cd963839f4b125145e47881def49b3c8b149820
Martin Goldman <martin@mgoldman.com>
2014-11-20 07:26:42 -0500

    test description validation in todo

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Todo, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Todo, type: :model do
+	it "should require a description" do
+		expect(Todo.new(description: '')).to_not be_valid
+	end
 end
