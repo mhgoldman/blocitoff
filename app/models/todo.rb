ad458ce9b9a855aac886fad5bb782b69a9653932
Martin Goldman <martin@mgoldman.com>
2014-11-21 13:44:16 -0500

    add # days left to display

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,4 +2,8 @@ class Todo < ActiveRecord::Base
 	belongs_to :user
 	validates :user, presence: true
 	validates :description, presence: true
+
+	def days_left
+		7 - ((Time.now - created_at) / 60 / 60 / 24).to_i
+	end
 end
