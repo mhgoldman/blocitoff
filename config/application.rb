3a84c2fdd3b50b6ee0633df1f5332bae544c1911
Martin Goldman <martin@mgoldman.com>
2014-11-23 22:28:19 -0500

    update devise views

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module Blocitoff
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # https://gist.github.com/dustMason/5817510
+    ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+        html = html_tag
+        html += %(<div class="form-field-error"> #{instance.error_message.to_a.to_sentence}</div>).html_safe unless html_tag =~ /^<label/
+        html.html_safe
+    end 
   end
 end
+
